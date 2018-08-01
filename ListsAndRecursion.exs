d3a3cc040f77b2c24bc7c367f4c39f7d9ea9f6e4
Yifeng Wang <git@yifengwang.me>
2018-07-31 22:07:36 -0700

    add span

--- ListsAndRecursion.exs
+++ ListsAndRecursion.exs
@@ -8,10 +8,12 @@ defmodule MyList do
   def caesar([], _n), do: []
   def caesar([head | tail], n) when head + n <= ?z, do: [head + n | caesar(tail, n)]
   def caesar([head | tail], n), do: [head + n - 26 | caesar(tail, n)]
+
+  def span(from, from), do: [from]
+  def span(from, to) when from < to, do: [from | span(from + 1, to)]
 end
 
 # IO.puts MyList.mapsum [1, 2, 3], &(&1 * &1)
-
 # IO.puts MyList.max [2, 3, 11, 9, 4]
-
-# IO.puts MyList.caesar('xyzabcd', 3)
+# IO.puts MyList.caesar 'xyzabcd', 3
+# IO.puts MyList.span 2, 10
