bccbf493706039b06fa1bab8deb4f739f5218320
Yifeng Wang <git@yifengwang.me>
2018-08-06 22:26:01 -0700

    add all?, each, filter, split, take

--- lists_and_recursion.exs
+++ lists_and_recursion.exs
@@ -17,3 +17,50 @@ end
 # IO.puts MyList.max [2, 3, 11, 9, 4]
 # IO.puts MyList.caesar 'xyzabcd', 3
 # IO.puts MyList.span 2, 10
+
+# ListsAndRecursion-5
+
+defmodule MyList2 do
+  def all?(list), do: all?(list, &(!!&1))
+  def all?([], _fun), do: true
+  def all?([head | tail], fun) do
+    if fun.(head) do
+      all?(tail, fun)
+    else
+      false
+    end
+  end
+
+  def each([], _fun), do: []
+  def each([head | tail], fun), do: [fun.(head) | each(tail, fun)]
+
+  def filter([], _fun), do: []
+  def filter([head | tail], fun) do
+    if fun.(head) do
+      [head | filter(tail, fun)]
+    else
+      filter(tail, fun)
+    end
+  end
+
+  # todo: handle negative count
+  def split(list, count), do: split(list, [], count)
+  defp split(tail, picked, 0), do: {Enum.reverse(picked), tail}
+  defp split([], picked, _), do: {Enum.reverse(picked), []}
+  defp split([head | tail], picked, count), do: split tail, [head | picked], count - 1
+
+  def take(list, count) do
+    {taken, _} = split(list, count)
+    taken
+  end
+
+end
+
+# IO.puts MyList2.all? [1, 2]
+# IO.puts MyList2.all? [1, 2], &(&1 > 1)
+# IO.inspect MyList2.each [1, 2], &(&1 + 1)
+# IO.inspect(MyList2.filter [0, 1, 2, 3, 4], &(&1 > 1))
+# IO.inspect MyList2.split [1, 2, 3, 4, 5], 2
+# IO.inspect MyList2.split [1, 2, 3, 4, 5], 5
+# IO.inspect MyList2.split [1, 2, 3, 4, 5], 8
+IO.inspect MyList2.take([1, 2, 3, 4, 5], 3)
